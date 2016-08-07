706c856ed0e22bcc03ea8e26bc38f709bd429edb
Alexey Donov <alexeydonov@me.com>
2016-08-07 09:24:00 +0300

    Initial Commit

---
+++ TagCollectionView/TagCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  TagCollectionView.h
+//  TagCollectionView
+//
+//  Created by Alexey Donov on 07.08.16.
+//  Copyright © 2016 Alexey Donov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TagCollectionView.
+FOUNDATION_EXPORT double TagCollectionViewVersionNumber;
+
+//! Project version string for TagCollectionView.
+FOUNDATION_EXPORT const unsigned char TagCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagCollectionView/PublicHeader.h>
+
+
